8250484a3d9eb37f272f936218ac2fd8cba45d5c
Your Name <you@example.com>
2016-11-27 23:15:59 +0100

    fixing socket

--- config/socket.js
+++ config/socket.js
@@ -1,8 +1,11 @@
 module.exports = function (io) {
     var connectedSockets = {};
     var connectedUsers = [];
+
     io.on('connection', function (socket) {
+        console.log(connectedUsers);
         socket.on('id', function (id) {
+            socket.userId = id;
             connectedSockets[id] = socket;
             connectedUsers.push(id);
             console.log(id, '  connected');
@@ -13,17 +16,20 @@ module.exports = function (io) {
         });
 
         socket.on('privateMessage', function (msg) {
+            console.log(connectedUsers);
             var data = msg.split(",");
-           // console.log(data);//from to msg
-            if (connectedSockets[connectedUsers[data[1]]]) {
-           //  console.log(data[0],'  send  ',data[2],' to  ',connectedUsers[data[1]]);
-             //   console.log(connectedUsers);
+            // console.log(data);//from to msg
 
-                connectedSockets[connectedUsers[data[1]]].emit('receivedMessage', data[2]);
+            if (connectedSockets[data[1]]) {
+                console.log(socket.userId, " send to ", connectedSockets[data[1]].userId);
+                connectedSockets[data[1]].emit('receivedMessage', data[2]);
             }
         });
-        // io.emit('some event', { for: 'everyone' });
+
         socket.on('disconnect', function () {
+            delete connectedSockets[socket.userId];
+            var index = connectedUsers.indexOf(socket.userId);
+            connectedUsers.splice(index, 1);
             console.log('user disconnected');
         });
     });
